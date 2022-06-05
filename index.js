689fa10469d8e4cb0cd65150a9a948ae7b92bbb1
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-05 21:30:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function  writeCards(name){
+let newArre=[];
+
+for(let i = 0; i < name.length ; i++){
+newArre.push("Thank you,"+name[i]+"for the wonderful birthday gift!")
+return newArre;
+}}
+function countDown10(){
+    let countup = 11;
+while (countup >0) {
+  console.log(countup--);
+}
+}
+function countDown(){
+    let countup = 4;
+while (countup >=0) {
+  console.log(countup--);
+}
+}
+
