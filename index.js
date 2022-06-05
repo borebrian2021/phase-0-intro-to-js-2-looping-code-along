bb615a6736570d568ad7806b113ba433f1ac429c
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-05 21:29:18 +0300

    commit tree 5dd8cfcf1c549dd6959c9e8c8e3a17b24e786d7b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function  writeCards(name){
+let newArre=[];
+
+for(let i = 0; i < name.length ; i++){
+newArre.push("Thank you,"+name[i]+"for the wonderful birthday gift!")
+return newArre;
+}}
+function countDown10(){
+    let countup = 11;
+while (countup >0) {
+  console.log(countup--);
+}
+
+let countup1 = 4;
+while (countup1 >=0) {
+  console.log(countup1--);
+}
+}
+
+
